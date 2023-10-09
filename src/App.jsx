387ad03dffb9cc4a0a7eee3d869be90009be84af
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-10-09 15:58:06 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Inicio from './componentes/paginas/Inicio'
+import SobreMim from './componentes/paginas/SobreMim'
 
 function App() {
 
-  return (
-    <>
-    <p>oi</p>
-    </>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<SobreMim/>}/>
+        <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
+    </BrowserRouter>
   )
+  
 }
 
 export default App
--- src/componentes/paginas/SobreMim/index.jsx
+++ src/componentes/paginas/SobreMim/index.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 function SobreMim() {
     return(
-        <h1>oi</h1>
+        <h1>Sobre mim</h1>
     )
 }
  export default SobreMim
