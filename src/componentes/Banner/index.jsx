a2cd556dd8a91e6f63df317ade1fe7580716d708
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-10-10 13:40:25 -0300

    Create index.jsx

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+function Banner() {
+    return (
+        <div className={styles.banner}> 
+
+            <img src="../../assets/Banner.png" alt="" />
+        </div>
+    )
+}
+
+export default Banner
