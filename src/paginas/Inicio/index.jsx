287a4e180e5f16943020f5914093f72939437165
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-10-10 13:40:41 -0300

    Update index.jsx

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,8 +1,14 @@
+import Banner from '../../componentes/Banner'
 import styles from './styles.module.css'
 
 function Inicio() {
-    return(
-        <h1>oi</h1>
+    return (
+        <section>
+            <Banner />
+            <h1>oi</h1>
+
+        </section>
+
     )
 }
- export default Inicio
+export default Inicio
