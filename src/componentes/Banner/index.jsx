38eb03e50bfcfcc1f29c6d4e9f482f7796883db9
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-10-10 14:44:17 -0300

    Update index.jsx

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -1,11 +1,34 @@
 import styles from './styles.module.css'
+import circuloColorido from '../../assets/circulo_colorido.png'
+import minhaFoto from '../../assets/minhaFoto.png'
+
 
 function Banner() {
     return (
-        <div className={styles.banner}> 
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal! Eu sou a Isabely, mas pode me chamar de Bel, futura desenvolvedora :)
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
 
-            <img src="../../assets/Banner.png" alt="" />
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto da Isabely"
+                />
+            </div>
         </div>
+
     )
 }
 
